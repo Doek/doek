7872fc4af1e957178e927942bef7a74536380eb7
Henrik Thorn <henrikthorn@it-kartellet.dk>
2011-03-06 16:46:52 +0100

    Addde settigns

--- www/sites/default/settings.php
+++ www/sites/default/settings.php
@@ -183,9 +183,9 @@ $databases = array (
   array (
     'default' => 
     array (
-      'database' => 'doek_drupal_7',
-      'username' => 'root',
-      'password' => 'rootisbad',
+      'database' => 'doek_doek2011',
+      'username' => 'doek',
+      'password' => 'yixWccPY',
       'host' => 'localhost',
       'port' => '',
       'driver' => 'mysql',
