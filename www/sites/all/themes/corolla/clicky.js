fb7125a30ea706f11a88866851497ea063f75978
Henrik Thorn <thorn@it-kartellet.dk>
2011-03-06 15:51:40 +0100

    Added new script

---
+++ www/sites/all/themes/corolla/clicky.js
@@ -0,0 +1,3 @@
+<script src="http://static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(66394470); }catch(err){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/66394470ns.gif" /></p></noscript>
