7f9d6e5f309a131d99d9d8978d1f120dd3d2678a
Henrik Thorn <henrikthorn@it-kartellet.dk>
2011-07-14 14:35:52 +0200

    added comments to front page view

--- www/sites/all/themes/corolla/views-view-fields--forside-portal.tpl.php
+++ www/sites/all/themes/corolla/views-view-fields--forside-portal.tpl.php
@@ -5,10 +5,16 @@
 $domain = domain_lookup($fields['domain_id']->content);
 
 echo "<h2><span class='site'><a href='http://".$domain['subdomain']."'>".$domain['sitename']."</a></span> | <span class='node-title'>".$fields['title']->content."</span></h2>";
+   
+echo "<span class='date-time'>";
 
 if(isset($fields['entity_id_2'])){
-   echo "<span class='date-time'>".$fields['entity_id_2']->content."</span>";
+  echo $fields['entity_id_2']->content;
 }
+if (isset($fields['comment_count'])){
+  echo " - ".$fields['comment_count']->content;
+}
+   echo "</span>";
 ?>
 	<div class='picture'>
 		<?php echo $fields['entity_id_1']->content; ?>
