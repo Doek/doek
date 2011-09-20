76a0af09007ffc520812531aaacdc548e03bf7c3
Henrik Thorn <henrikthorn@it-kartellet.dk>
2011-09-20 18:22:47 +0200

    Upgraded google plusone

--- www/sites/all/modules/google_plusone/google_plusone.admin.js
+++ www/sites/all/modules/google_plusone/google_plusone.admin.js
@@ -5,8 +5,11 @@
 
       var $preview = $('#google_plusone_preview').show();
 
-      // In the first load.
-      var $sizeForm = $('#edit-google-plusone-button-settings-size');
+      // Selectors with complex matches to be able to re-use it
+      // in admin page and block settings page.
+      var $sizeForm = $('div[id$="-size"]');
+      var $widthInput = $('input[id$="-width"]');
+
       var size = $sizeForm.find(':checked').val();
       $preview.find('#google_plusone_' + size).addClass('active_size').show();
 
@@ -15,6 +18,15 @@
          var size = $(this).find(':checked').val();
          $preview.find('.active_size').hide();
          $preview.find('#google_plusone_' + size).addClass('active_size').show();
+      });
+
+      $widthInput.bind('keyup', function(){
+        var newWidth = $(this).val();
+        var $container = $preview.find('.g-inline').empty();
+        var sizes = ['small','medium','standard','tall'];
+        $container.each(function(i){
+          gapi.plusone.render(this, {'size':sizes[i],'annotation':'inline','href':'http://drupal.org','width': parseInt(newWidth)});
+        });
       });
     }
   };
