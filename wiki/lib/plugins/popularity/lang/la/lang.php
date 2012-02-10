155d64c0642507ba07363782c8689e51b759834c
Henrik Thorn <henrikthorn@gmail.com>
2012-02-10 11:37:03 +0100

    Removed wiki, which should be updated at some point

--- wiki/lib/plugins/popularity/lang/la/lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Latin language file
- *
- */
-$lang['name']                  = 'Index fauoris popularis (multum tempus quaerere potest)';
-$lang['submit']                = 'Missum die';
